a77d5de4dc7eb045862eab7bb1a4d027e1f82f93
tarung <tarun.gupta@technogica.com>
2013-07-11 19:38:34 +0530

    Refactoring.

---
+++ Util/src/com/blockwithme/fn/util/Functor.java
@@ -0,0 +1,11 @@
+package com.blockwithme.fn.util;
+
+/** The Functor interface extended by all the Function interfaces
+ * that are generated PrimitiveFunc Generator
+ *
+ * @author tarung
+ *
+ * */
+public interface Functor {
+    // Just a marker interface
+}
